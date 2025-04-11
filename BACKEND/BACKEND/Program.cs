adae7d2a1bfe05c0193abe6be4c11d80b579b8ce
jancris100 <73757410+jancris100@users.noreply.github.com>
2025-04-11 15:48:59 -0600

    feat: permisos cors

--- BACKEND/BACKEND/Program.cs
+++ BACKEND/BACKEND/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
